63d1edb81f3e849c22099ab683ddb43ecbd4bbf3
KurlykovDanila <56872251+kurlykovdanila@users.noreply.github.com>
2020-04-03 23:37:51 +0700

    fix bag with update page

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from flask_login import LoginManager, login_required, logout_user
 from flask_restful import Api
 from werkzeug.utils import redirect
 from flask import render_template
+from flask import make_response
 
 from data import db_session
 from data.users import User
+
+import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'af1f2ed264b7a6b18b84971091cbaceea33697bf3b80ad5cd495898c8ced0a2d09b1e8012a0' \
@@ -31,12 +34,15 @@ def logout():
 
 @app.route('/start')
 def start():
-    return render_template("main.html")
+    response = make_response(render_template("main.html", version=random.randint(0, 10 ** 5)))
+    response.headers['Cache-Control'] = 'no-cache, no-store'
+    response.headers['Pragma'] = 'no-cache'
+    return response
 
 
 def main():
     db_session.global_init("db/passwords.sqlite")
-    app.run(port=5000, host='127.0.0.1')
+    app.run(port=5000, host='192.168.1.114')
 
 
 if __name__ == '__main__':
